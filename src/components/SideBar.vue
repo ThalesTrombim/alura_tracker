d13b1caf63ea0fe0f892f03ded7cea08416d1cab
ThalesTrombim <thalestr@outlook.com>
2022-05-11 22:11:57 -0300

    final da aula 1, gerenciar roteamento

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,6 +6,22 @@
     <button class="button" @click='switchTheme'>
       {{ textButton }}
     </button>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/"  class="link">
+            <i class="fas fa-tasks"></i>
+            Tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            Projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -42,6 +58,19 @@
     padding: 1rem;
     text-align: center;
   }
+  .panel li {
+    margin: 8px 0;
+  }
+  .link {
+    color: #FFF;
+  }
+  .link:hover {
+    color: #FAF0CA;
+  }
+  .link.router-link-active {
+    color: #FAF0CA;
+  }
+
   @media only screen and (max-width: 768px) {
     header {
       padding: 2.5rem;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,19 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Tasks from '../views/Tasks.vue';
+import Projects from '../views/Projects.vue';
 
-const routes:RouteRecordRaw[] = [{
-  path: '/',
-  name: 'Tarefas',
-  component: Tasks
-}];
+const routes:RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Tarefas',
+    component: Tasks
+  },
+  {
+    path: '/projetos',
+    name: 'Projetos',
+    component: Projects
+  }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
